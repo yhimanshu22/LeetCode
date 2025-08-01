bcaa94991644b773b4f6d4d2e74a0967d4c2146e
Himanshu Yadav <himu09854@gmail.com>
2025-08-02 02:48:48 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 2087
          Language: MySQL
          Runtime: 617 ms
          Runtime percentile: 77.10
          Memory: 0 MB
          Memory percentile: 100.00

--- 2087. Confirmation Rate/Solution.sql
+++ 2087. Confirmation Rate/Solution.sql
@@ -1,7 +1,5 @@
-SELECT 
-  a.user_id,
-  round(avg(if(b.action = 'confirmed',1.00 , 0)),2) AS confirmation_rate
-FROM Signups AS a
-LEFT JOIN Confirmations AS b
-  ON a.user_id = b.user_id
-GROUP BY a.user_id
+select a.user_id ,round(avg(if(b.action = 'confirmed',1.00,0)),2) as confirmation_rate
+from Signups as a
+left join Confirmations as b
+on a.user_id = b.user_id
+group by user_id
