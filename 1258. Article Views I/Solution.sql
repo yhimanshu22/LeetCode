761c5a1bc055fa371b61e0636d2cc934392509b2
Himanshu Yadav <himu09854@gmail.com>
2025-08-04 03:05:47 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 1258
          Language: MySQL
          Runtime: 373 ms
          Runtime percentile: 93.71
          Memory: 0 MB
          Memory percentile: 100.00

--- 1258. Article Views I/Solution.sql
+++ 1258. Article Views I/Solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select distinct author_id as id from Views
-where author_id = viewer_id 
-order by id asc;
+select author_id as id  from Views
+where author_id = viewer_id
+group by author_id
+order by author_id
