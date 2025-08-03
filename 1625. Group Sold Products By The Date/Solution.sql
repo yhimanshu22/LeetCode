8ab8371d6fbfb1b0000988e1d9b04ddecfbd987a
Himanshu Yadav <himu09854@gmail.com>
2025-08-04 02:51:55 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 1625
          Language: MySQL
          Runtime: 394 ms
          Runtime percentile: 97.10
          Memory: 0 MB
          Memory percentile: 100.00

--- 1625. Group Sold Products By The Date/Solution.sql
+++ 1625. Group Sold Products By The Date/Solution.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select 
-sell_date,
-count(distinct product) as num_sold,
-group_concat(distinct product order by product asc separator ',') as products from Activities
+select sell_date,count(distinct product) as num_sold ,
+ group_concat(distinct product separator ',') as products from Activities
 group by sell_date
-order by sell_date asc
+order by sell_date
+
