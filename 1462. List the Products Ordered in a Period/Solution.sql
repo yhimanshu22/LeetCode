409cda62b15fb55eb84c0ce304886bca089f2a52
Himanshu Yadav <himu09854@gmail.com>
2025-08-03 23:04:16 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 1462
          Language: MySQL
          Runtime: 655 ms
          Runtime percentile: 86.02
          Memory: 0 MB
          Memory percentile: 100.00

--- 1462. List the Products Ordered in a Period/Solution.sql
+++ 1462. List the Products Ordered in a Period/Solution.sql
@@ -1,7 +1,8 @@
-SELECT a.product_name, SUM(b.unit) AS unit
-FROM Products a
-JOIN Orders b
-  ON a.product_id = b.product_id
+/* Write your T-SQL query statement below */
+
+SELECT product_name ,SUM(unit) AS unit
+FROM products p INNER JOIN  orders o 
+ON p.product_id =o.product_id
 WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY a.product_id 
-HAVING SUM(b.unit) >= 100;
+GROUP BY p.product_id,p.product_name 
+HAVING SUM(unit)>=100
