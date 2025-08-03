f16b65a65d20b25be27c7f6b21b13db6bfede776
Himanshu Yadav <himu09854@gmail.com>
2025-08-03 23:02:49 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 1462
          Language: MySQL
          Runtime: 696 ms
          Runtime percentile: 65.42
          Memory: 0 MB
          Memory percentile: 100.00

---
+++ 1462. List the Products Ordered in a Period/Solution.sql
@@ -0,0 +1,7 @@
+SELECT a.product_name, SUM(b.unit) AS unit
+FROM Products a
+JOIN Orders b
+  ON a.product_id = b.product_id
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY a.product_id ,a.product_name
+HAVING SUM(b.unit) >= 100;
