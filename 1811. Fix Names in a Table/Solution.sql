9d0c8eadd37073fe14048b581bd17b2b382dc797
Himanshu Yadav <himu09854@gmail.com>
2025-08-04 02:38:55 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 1811
          Language: MySQL
          Runtime: 690 ms
          Runtime percentile: 27.06
          Memory: 0 MB
          Memory percentile: 100.00

--- 1811. Fix Names in a Table/Solution.sql
+++ 1811. Fix Names in a Table/Solution.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-select user_id ,concat(upper(left(name,1)),lower(substring(name,2))) as name
-from Users
+select user_id , concat(upper(left(name,1)),lower(substring(name,2))) as name from Users
 order by user_id
