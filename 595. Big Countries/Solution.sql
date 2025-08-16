5bcb985e59814add465fff37a84021a84e8f536b
Himanshu Yadav <himu09854@gmail.com>
2025-08-17 00:35:49 +0530

    MySQL Solution
          Submission Statistics:
          Question #: 595
          Language: MySQL
          Runtime: 282 ms
          Runtime percentile: 56.43
          Memory: 0 MB
          Memory percentile: 100.00

--- 595. Big Countries/Solution.sql
+++ 595. Big Countries/Solution.sql
@@ -1,2 +1,3 @@
-select name,population ,area from World
-where area >= 3000000 or population >= 25000000
+# Write your MySQL query statement below
+select name , population , area from World
+where area >= 3000000  or population >= 25000000;
