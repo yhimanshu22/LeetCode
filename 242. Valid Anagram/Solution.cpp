2c3f86a40091cf99a7349cd46e525e8567f49f29
Himanshu Yadav <himu09854@gmail.com>
2025-08-26 03:04:55 +0530

    C++ Solution
          Submission Statistics:
          Question #: 242
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.676 MB
          Memory percentile: 74.16

---
+++ 242. Valid Anagram/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())return false;
+
+        vector<int> count(26,0);
+        
+        for(int i=0;i<s.size();i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for(int num:count){
+        if(num != 0)return false;
+        }
+        return true;
+
+    }
+};
